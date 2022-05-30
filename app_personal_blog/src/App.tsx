e86cebf1edc49e26d19d9ab6a440c40da4c10101
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-05-30 22:39:14 +0300

    start hw_41

--- app_personal_blog/src/App.tsx
+++ app_personal_blog/src/App.tsx
@@ -3,15 +3,16 @@ import './App.css';
 //import { FirstPage } from './pages/first-page/FirstPage';
 //import { LoginPage } from './pages/login-page/LoginPage';
 //import { LoginConfirmation } from './pages/loginConfirmation-page/LoginConfirmation';
-//import { RegistrationPage } from './pages/registration-page/RegistrationPage';
+import { RegistrationPage } from './pages/registration-page/RegistrationPage';
 //import { RegistrationConfirmation } from './pages/registrationConfirmation-page/RegistrationConfirmation';
 //import { SelectedPostPage } from './pages/selected-post-page/SelectedPostPage';
-import { AllPostPage } from './pages/all-posts-page/AllPostPage';
+//import { AllPostPage } from './pages/all-posts-page/AllPostPage';
+//import { InformationPage } from './pages/information-page/InformationPage';
 
 function App() {
   return (
     <div className="app">
-      <AllPostPage></AllPostPage>
+      <RegistrationPage></RegistrationPage>
     </div>
   );
 }
