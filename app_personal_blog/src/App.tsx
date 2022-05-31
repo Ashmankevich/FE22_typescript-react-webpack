7dee56dea81bff4408bbad83cc4b35cb29670fb2
Andrew Ashmankevich <Ashmankevich.a@gmail.com>
2022-05-31 21:33:24 +0300

    init routing

--- app_personal_blog/src/App.tsx
+++ app_personal_blog/src/App.tsx
@@ -3,16 +3,23 @@ import './App.css';
 //import { FirstPage } from './pages/first-page/FirstPage';
 //import { LoginPage } from './pages/login-page/LoginPage';
 //import { LoginConfirmation } from './pages/loginConfirmation-page/LoginConfirmation';
-import { RegistrationPage } from './pages/registration-page/RegistrationPage';
+//import { RegistrationPage } from './pages/registration-page/RegistrationPage';
 //import { RegistrationConfirmation } from './pages/registrationConfirmation-page/RegistrationConfirmation';
 //import { SelectedPostPage } from './pages/selected-post-page/SelectedPostPage';
 //import { AllPostPage } from './pages/all-posts-page/AllPostPage';
 //import { InformationPage } from './pages/information-page/InformationPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AppPages } from './types';
+import { LoginPage } from './pages/login-page/LoginPage';
 
 function App() {
   return (
     <div className="app">
-      <RegistrationPage></RegistrationPage>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppPages.LOGIN} element={LoginPage}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ app_personal_blog/src/types.ts
@@ -0,0 +1,9 @@
+export enum AppPages {
+   LOGIN = "/login",
+   REGISTRATION = "/registration",
+   LANDING = "/landing",
+   POSTS = "/posts",
+   POSTS_ITEM = "/posts/:id",
+   RESET_PASSWORD = "/reset-password",
+   SUCCESS_PAGE = "/success-page",
+ }
